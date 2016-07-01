6f6e4e1a94a3f1133566a7f34979491951274438
Peter <petercort@gmail.com>
2016-07-01 15:17:00 -0400

    updating with new code lines

--- multiverseFinder.py
+++ multiverseFinder.py
@@ -1,8 +1,18 @@
 #! /usr/bin/python
 import json
 
-print "Enter a card name",
-cardMatch = raw_input()
+#print "Enter a card name:",
+#cardMatch = raw_input()
+
+cardNames = []
+cardInt = 0
+print "Enter path of list:",
+listDirectory = raw_input()
+f=open(listDirectory)
+data=f.read().splitlines()
+for line in data:
+	cardNames.append(line)
+f.close()
 
 json_file='AllSets.json'
 json_data=open(json_file)
@@ -15,15 +25,20 @@ for val in data:
 	setCode=(data[val]["code"])
 	sets.append(str(setCode))
 
-for val in sets:
-	cardNumber=0
-	x=0
-	numCards=len(data[val]["cards"])
-	while cardNumber < numCards:
-		cardName=data[val]["cards"][x]["name"]
-		if cardName==cardMatch:
-			print("Card Name Matched")
-			print("http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid="+str(data[val]["cards"][x]["multiverseid"]))
-			quit()
-		cardNumber=cardNumber+1
-		x=x+1
+while cardInt < len(cardNames):
+	cardName = cardNames[cardInt]
+	for val in sets:
+		cardNumber=0
+		x=0
+		y=0
+		numCards=len(data[val]["cards"])
+		while cardNumber < numCards:
+			cardMatch=data[val]["cards"][x]["name"]
+			if cardName==cardMatch:
+				print("Card Name Matched")
+				print("http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid="+str(data[val]["cards"][x]["multiverseid"]))
+			cardNumber=cardNumber+1
+			x=x+1
+			break
+		break
+cardInt = cardInt+1
